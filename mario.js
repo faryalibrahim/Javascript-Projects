669d2354d9e44eee3684bc1976ea9ca450572a04
faryalibrahim <faryal_ibrahim2000@hotmail.com>
2022-04-13 19:32:50 +0100

    wip for implementing pyramid

--- mario.js
+++ mario.js
@@ -5,8 +5,29 @@ Number cant be greater than 20
 Pyramid will be left aligned 
 */
 
+// naive implementation 
+// function pyramid(heightValue) {
+//     console.log('heightValue', heightValue, heightValue === 2)
+//     if(heightValue === 1) {
+//         return `#`;
+//     }
+//     if(heightValue === 2) {
+//         const height2 = 
+//     `
+//     #
+//     ##
+//     `
+//         return height2;
+//     }
+// };
 
-function pyramid (value) {
-    return '#';
-};
+function pyramid(heightValue) {
+    let result = ''
+    
+    for (let y = 1; y <= heightValue; y++) {
+        
+       var line = `#`.repeat(y);
+  }
+  return line
+}
 module.exports = pyramid;
--- mario.test.js
+++ mario.test.js
@@ -1,5 +1,14 @@
 const pyramid = require("./mario");
 
-test ("when height is 1 pyramid is #", () => {
+test.only("when height is 1 pyramid is #", () => {
     expect (pyramid(1)).toBe('#');
+})
+
+test ("when the height is 2, pyramid is '#', /n '##'", () => {
+    const result = 
+    `
+    #
+    ##
+    `
+    expect (pyramid(2)).toBe(result);
 })
